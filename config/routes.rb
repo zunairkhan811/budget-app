57189de27a0c442454dc770b2745fa507a06f0dd
Muhammad Zunair khan <zunairkhan811@gmail.com>
2024-01-09 03:09:49 +0500

    create groups controller and groups index.html file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  root "groups#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
