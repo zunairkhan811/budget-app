8be4988da1827ed9575908443eab5603931122ae
Muhammad Zunair khan <zunairkhan811@gmail.com>
2024-01-09 02:59:24 +0500

    Generate devise views for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :groups dependent: :destroy
-    has_many :foods dependent: :destroy
-    validates :name, presence: true
+  has_many :groups, dependent: :destroy
+  has_many :foods, dependent: :destroy
+  validates :name, presence: true
 end
