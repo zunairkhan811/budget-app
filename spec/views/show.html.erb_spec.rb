7ebe7f29c9d4db8b0aa5addc83d34bcbfa86a445
Muhammad Zunair khan <zunairkhan811@gmail.com>
2024-01-11 23:58:58 +0500

    make tests for groups show.html.erb

--- spec/views/show.html.erb_spec.rb
+++ spec/views/show.html.erb_spec.rb
@@ -1,35 +1,35 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Groups Show Page', type: :system do
-#   let(:user1) { User.create(name: 'John Doe', email: 'john@example.com', password: 'password', password_confirmation: 'password') }
-#   let!(:group1) { Group.create(name: 'Delicious Group', icon: 'icon_url_1', user: user1) }
-#   let!(:food1) { Food.create(name: 'Tasty Food 1', amount: 20.5, author: user1, group_id: group1) }
+RSpec.describe 'Groups Show Page', type: :system do
+  let(:user1) { User.create(name: 'John Doe', email: 'john@example.com', password: 'password', password_confirmation: 'password') }
+  let!(:group1) { Group.create(name: 'Delicious Group', icon: 'icon_url_1', user: user1) }
+  let!(:food1) { Food.create(name: 'Tasty Food 1', amount: 20.5, author_id: user1.id, group_id: group1.id) }
 
-#   before do
-#     login_as(user1, scope: :user)
-#     visit group_path(group1)
-#   end
+  before do
+    login_as(user1, scope: :user)
+    visit group_path(group1)
+  end
 
-#   it 'displays details of the group and its foods' do
-#     expect(page).to have_content('Delicious Group')
-#     expect(page).to have_content("$#{group1.foods.sum(:amount)}")
-#     expect(page).to have_content('11 January 2024') 
-#     expect(page).to have_content(food1.name)
-#   end
+  it 'displays details of the group and its foods' do
+    expect(page).to have_content('Delicious Group')
+    expect(page).to have_content("$#{group1.foods.sum(:amount)}")
+    expect(page).to have_content('11 January 2024') 
+  end
 
-#   it 'allows editing the group' do
-#     click_link('Edit')
-#     expect(page).to have_content('Update Restaurant')
-#   end
+  it 'allows editing the group' do
+    click_link('Edit')
+    expect(page).to have_content('Update Restaurant')
+  end
 
-#   it 'displays details of the foods in the group' do
-#     within('.restaurant-card-container') do
-#       expect(page).to have_content(food1.name)
-#     end
-#   end
+  it 'displays details of the foods in the group' do
+    within('.food-container') do
+        expect(page).to have_content(food1.name)
+    end
+  end
 
-#   it 'allows adding a new food to the group' do
-#     click_link('Add new Food')
-#     expect(page).to have_content('Food was successfully added.')
-#   end
-# end
+  it 'allows adding a new food to the group' do
+    within('.my-footer') do
+        expect(page).to have_link('Add new Food')
+    end
+  end
+end
